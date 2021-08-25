700d9d56d67b6603b7e6d08776399ae24fe97d42
Akif-jpg <akifesadi193@gmail.com>
2021-08-25 17:41:09 +0300

    updated desktop entry

--- src/com/game/xtetrix/DesktopLauncher.java
+++ src/com/game/xtetrix/DesktopLauncher.java
@@ -4,9 +4,6 @@ package com.game.xtetrix;
 public class DesktopLauncher {
 	public static void main(String[] args) {
 		PageManager pageManager = new PageManager();	
-		pageManager.intializer();
-		Object obj = new Object();
-		System.out.println(DesktopLauncher.class.getName());
-        	
+		pageManager.intializer();        	
 	}
 }
